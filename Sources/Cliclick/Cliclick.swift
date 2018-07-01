4c219919bfa08dd01aa219924eaf894de6595c3b
Jeremy Bannister <jeremy.a.bannister@gmail.com>
2018-07-01 04:12:31 -0700

    Better API for executing commands while holding down keys

--- Sources/Cliclick/Cliclick.swift
+++ Sources/Cliclick/Cliclick.swift
@@ -10,19 +10,16 @@ import Foundation
 import Shell
 import OSInteractor
 
+// MARK: - Initial Declaration
 public struct Cliclick {
   private let shell: Shell
   
   public init (shell: Shell) {
     self.shell = shell
   }
-  
-  private func execute (_ commands: String) {
-    let arguments = commands.trimmingCharacters(in: .whitespacesAndNewlines).components(separatedBy: " ")
-    shell.staggarExecution(path: "/usr/local/bin/cliclick", arguments: arguments)
-  }
 }
 
+// MARK: - Public API
 extension Cliclick {
   public func move (to x: Double, _ y: Double) {
     execute("m:\(formattedCoordinates(x, y))")
@@ -37,63 +34,91 @@ extension Cliclick {
     execute("dc:\(formattedCoordinates(x, y))")
   }
   public func press (_ pressableKey: PressableKey) {
-    execute("kp:\(identifierFor(pressableKey))")
+    execute("kp:\(pressableKey.identifier)")
   }
-  public func hold (_ holdableKeys: [HoldableKey], andDoCommand commandString: String) {
-    let keysString = holdableKeys.map({ identifierFor($0) }).joined(separator: ",")
-    execute("kd:\(keysString) \(commandString) ku:\(keysString)")
+  
+  public func hold (_ holdableKey: HoldableKey) {
+    hold(keys: [holdableKey])
   }
-  public func hold (_ holdableKey: HoldableKey, andDoCommand commandString: String) {
-    self.hold([holdableKey], andDoCommand: commandString)
+  public func hold (keys: [HoldableKey]) {
+    execute("kd:\(keys.formattedForCommand)")
   }
-  public func hold (_ holdableKey: HoldableKey, _ commandString: String) {
-    self.hold(holdableKey, andDoCommand: commandString)
+  
+  public func unhold (_ holdableKey: HoldableKey) {
+    unhold(keys: [holdableKey])
+  }
+  public func unhold (keys: [HoldableKey]) {
+    execute("kp:\(keys.formattedForCommand)")
+  }
+  
+  public func whileHolding (_ key: HoldableKey, do action: ()->()) {
+    whileHolding(keys: [key], do: action)
+  }
+  public func whileHolding (keys: [HoldableKey], do action: ()->()) {
+    hold(keys: keys)
+    action()
+    unhold(keys: keys)
   }
 }
 
+// MARK: - Private
 private extension Cliclick {
+  func execute (_ commands: String) {
+    let arguments = commands.trimmingCharacters(in: .whitespacesAndNewlines).components(separatedBy: " ")
+    shell.staggarExecution(path: "/usr/local/bin/cliclick", arguments: arguments)
+  }
+  
   func formattedCoordinates (_ x: Double, _ y: Double) -> String {
     return "\(Int(x)),\(Int(y))"
   }
-  
-  func identifierFor (_ pressableKey: PressableKey) -> String {
-    switch pressableKey {
-    case .leftArrow: return "arrow-left"
-    case .rightArrow: return "arrow-right"
-    case .upArrow: return "arrow-up"
-    case .downArrow: return "arrow-down"
-    case .delete: return "delete"
-    case .return: return "return"
-    case .enter: return "enter"
-    case .esc: return "esc"
-    case .f1: return "f1"
-    case .f2: return "f2"
-    case .f3: return "f3"
-    case .f4: return "f4"
-    case .f5: return "f5"
-    case .f6: return "f6"
-    case .f7: return "f7"
-    case .f8: return "f8"
-    case .f9: return "f9"
-    case .f10: return "f10"
-    case .f11: return "f11"
-    case .f12: return "f12"
-    case .f13: return "f13"
-    case .f14: return "f14"
-    case .f15: return "f15"
-    case .f16: return "f16"
-    case .space: return "space"
-    case .tab: return "tab"
-    }
-  }
-  
-  func identifierFor (_ holdableKey: HoldableKey) -> String {
-    switch holdableKey {
+}
+
+// MARK: - Convenient Local Extensions
+private extension Array where Element == HoldableKey {
+  var formattedForCommand: String { return self.map({ $0.identifier }).joined(separator: ",") }
+}
+
+private extension HoldableKey {
+  var identifier: String {
+    switch self {
     case .alt: return "alt"
     case .cmd: return "cmd"
     case .ctrl: return "ctrl"
     case .fn: return "fn"
     case .shift: return "shift"
+    }
+  }
+}
+
+private extension PressableKey {
+  var identifier: String {
+    switch self {
+      case .leftArrow: return "arrow-left"
+      case .rightArrow: return "arrow-right"
+      case .upArrow: return "arrow-up"
+      case .downArrow: return "arrow-down"
+      case .delete: return "delete"
+      case .return: return "return"
+      case .enter: return "enter"
+      case .esc: return "esc"
+      case .f1: return "f1"
+      case .f2: return "f2"
+      case .f3: return "f3"
+      case .f4: return "f4"
+      case .f5: return "f5"
+      case .f6: return "f6"
+      case .f7: return "f7"
+      case .f8: return "f8"
+      case .f9: return "f9"
+      case .f10: return "f10"
+      case .f11: return "f11"
+      case .f12: return "f12"
+      case .f13: return "f13"
+      case .f14: return "f14"
+      case .f15: return "f15"
+      case .f16: return "f16"
+      case .space: return "space"
+      case .tab: return "tab"
     }
   }
 }
