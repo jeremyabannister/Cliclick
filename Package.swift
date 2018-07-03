7fd0ec6bd03c79abffb1f81e84dc2901f9440ae4
Jeremy Bannister <jeremy.a.bannister@gmail.com>
2018-07-03 01:10:27 -0700

    Updated package dependencies to use github URLs rather than local ones

--- Package.swift
+++ Package.swift
@@ -13,8 +13,8 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "../Shell", from: "0.1.0"),
-        .package(url: "../OSInteractor", from: "0.1.0"),
+        .package(url: "https://github.com/jeremyabannister/Shell.git", from: "0.1.0"),
+        .package(url: "https://github.com/jeremyabannister/OSInteractor.git", from: "0.1.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
